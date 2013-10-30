5d13d37b1692479b86188f70b80a5b6417c1ae50
Andrei Picus <office.nightcrawler@gmail.com>
2013-10-30 15:54:38 +0200

    Cleaned up tests_multiple_paths.
    Moved wavy setup code to module setup function and cleaned var statements.

--- tests/tests_multiple_paths.js
+++ tests/tests_multiple_paths.js
@@ -1,15 +1,19 @@
-module("multiple_paths");
+module("multiple_paths", {
+		setup: function() {
+				var fixture = $("#qunit-fixture");
+				var w = $("<div></div>").attr("id", "w");
+				w.appendTo(fixture);
+				w.wavy({
+						path: [
+								["line", 5, 0, 0, 300, 300],
+								["line", 5, 350, 300, 350, 0]
+						]
+				});
+		}
+});
 test("test_create_multiple_paths", function() {
-		var fixture = $("#qunit-fixture");
-		var w = $("<div></div>");
-		w.appendTo(fixture);
-		w.wavy({
-				path: [
-						["line", 5, 0, 0, 300, 300],
-						["line", 5, 350, 300, 350, 0]
-				]
-		});
-
+		var fixture = $("#qunit-fixture"),
+				w = $("#w");
 		equal($(".wavy-slot", w).length, 10);
 
 		var expectedX = [0, 75, 150, 225, 300, 350, 350, 350, 350, 350],
@@ -26,17 +30,9 @@ test("test_create_multiple_paths", function() {
 });
 
 test("test_drag_between_multiple_paths_first_to_last", function() {
-		var fixture = $("#qunit-fixture");
-		var w = $("<div></div>");
-		w.appendTo(fixture);
-		w.wavy({
-				path: [
-						["line", 5, 0, 0, 300, 300],
-						["line", 5, 350, 300, 350, 0]
-				]
-		});
-
-		var item = $("<div></div>").text("test").addClass("test-item");
+		var fixture = $("#qunit-fixture"),
+				w = $("#w"),
+				item = $("<div></div>").text("test").addClass("test-item");
 		w.wavy("addItem", item);
 
 		dd(item, $(".wavy-slot:last", w));
@@ -45,17 +41,9 @@ test("test_drag_between_multiple_paths_first_to_last", function() {
 });
 
 test("test_drag_between_multiple_paths_last_to_first", function() {
-		var fixture = $("#qunit-fixture");
-		var w = $("<div></div>");
-		w.appendTo(fixture);
-		w.wavy({
-				path: [
-						["line", 5, 0, 0, 300, 300],
-						["line", 5, 350, 300, 350, 0]
-				]
-		});
-
-		var item = $("<div></div>").text("test").addClass("test-item");
+		var fixture = $("#qunit-fixture"),
+				w = $("#w"),
+				item = $("<div></div>").text("test").addClass("test-item");
 		w.wavy("addItem", item, 9);
 
 		dd(item, $(".wavy-slot:first", w));
@@ -64,17 +52,9 @@ test("test_drag_between_multiple_paths_last_to_first", function() {
 });
 
 test("test_drag_between_multiple_paths_first_to_first_of_2nd_path", function() {
-		var fixture = $("#qunit-fixture");
-		var w = $("<div></div>");
-		w.appendTo(fixture);
-		w.wavy({
-				path: [
-						["line", 5, 0, 0, 300, 300],
-						["line", 5, 350, 300, 350, 0]
-				]
-		});
-
-		var item = $("<div></div>").text("test").addClass("test-item");
+		var fixture = $("#qunit-fixture"),
+				w = $("#w"),
+				item = $("<div></div>").text("test").addClass("test-item");
 		w.wavy("addItem", item);
 
 		dd(item, $(".wavy-slot:eq(5)", w));
@@ -83,17 +63,9 @@ test("test_drag_between_multiple_paths_first_to_first_of_2nd_path", function() {
 });
 
 test("test_drag_between_multiple_paths_last_to_first_of_2nd_path", function() {
-		var fixture = $("#qunit-fixture");
-		var w = $("<div></div>");
-		w.appendTo(fixture);
-		w.wavy({
-				path: [
-						["line", 5, 0, 0, 300, 300],
-						["line", 5, 350, 300, 350, 0]
-				]
-		});
-
-		var item = $("<div></div>").text("test").addClass("test-item");
+		var fixture = $("#qunit-fixture"),
+				w = $("#w"),
+				item = $("<div></div>").text("test").addClass("test-item");
 		w.wavy("addItem", item, 9);
 
 		dd(item, $(".wavy-slot:eq(5)", w));
@@ -102,17 +74,9 @@ test("test_drag_between_multiple_paths_last_to_first_of_2nd_path", function() {
 });
 
 test("test_drag_between_multiple_paths_last_of_1st_path_to_first_of_2nd_path", function() {
-		var fixture = $("#qunit-fixture");
-		var w = $("<div></div>");
-		w.appendTo(fixture);
-		w.wavy({
-				path: [
-						["line", 5, 0, 0, 300, 300],
-						["line", 5, 350, 300, 350, 0]
-				]
-		});
-
-		var item = $("<div></div>").text("test").addClass("test-item");
+		var fixture = $("#qunit-fixture"),
+				w = $("#w"),
+				item = $("<div></div>").text("test").addClass("test-item");
 		w.wavy("addItem", item, 4);
 
 		dd(item, $(".wavy-slot:eq(5)", w));
@@ -121,17 +85,9 @@ test("test_drag_between_multiple_paths_last_of_1st_path_to_first_of_2nd_path", f
 });
 
 test("test_drag_between_multiple_paths_last_of_1st_path_to_last_of_2nd_path", function() {
-		var fixture = $("#qunit-fixture");
-		var w = $("<div></div>");
-		w.appendTo(fixture);
-		w.wavy({
-				path: [
-						["line", 5, 0, 0, 300, 300],
-						["line", 5, 350, 300, 350, 0]
-				]
-		});
-
-		var item = $("<div></div>").text("test").addClass("test-item");
+		var fixture = $("#qunit-fixture"),
+				w = $("#w"),
+				item = $("<div></div>").text("test").addClass("test-item");
 		w.wavy("addItem", item, 4);
 
 		dd(item, $(".wavy-slot:last", w));
